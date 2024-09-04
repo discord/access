67f418a5c070c1a036595e00c18e703861df3887
Peter Collins <somethingnew2-0@users.noreply.github.com>
2024-09-04 18:51:19 -0400

    Add tests

--- api/views/health_check_views.py
+++ api/views/health_check_views.py
@@ -1,12 +1,20 @@
 from typing import Any
 
 from flask import Blueprint
+from sqlalchemy import text
 
-bp_name = "health_check"
+from api.extensions import db
+
+bp_name = "api-health-check"
 bp_url_prefix = "/api/healthz"
 bp = Blueprint(bp_name, __name__, url_prefix=bp_url_prefix)
 
 
-@bp.route("/", methods=["GET"])
+@bp.route("", methods=["GET"])
 def health_check() -> Any:
+    try:
+        db.session.execute(text("SELECT 1"))
+    except Exception as e:
+        return {"status": "error", "error": str(e)}, 500
+
     return {"status": "ok"}, 200
