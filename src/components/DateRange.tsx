acdac320ac95b3522a659c5203bfcaca5d2a33dd
amyjchen <amy.chen@discordapp.com>
2024-10-30 12:55:28 -0700

    remove unnecessary div on datepicker

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -91,26 +91,24 @@ function ButtonField(props: ButtonFieldProps) {
 
   return (
     <Box component={'div'} sx={{position: 'relative'}}>
-      <Box component={'div'}>
-        <Button
-          variant="outlined"
-          size="medium"
-          id={id}
-          disabled={disabled}
-          ref={ref}
-          aria-label={ariaLabel}
-          onClick={() => setOpen?.((prev) => !prev)}
-          sx={{
-            color: theme.palette.text.secondary,
-            borderColor: theme.palette.action.disabled,
-            minWidth: '245px',
-            fontSize: '15px',
-            position: 'relative',
-            zIndex: '1',
-          }}>
-          {displayString} <EventIcon sx={{marginLeft: '20px', color: theme.palette.text.secondary}} />
-        </Button>
-      </Box>
+      <Button
+        variant="outlined"
+        size="medium"
+        id={id}
+        disabled={disabled}
+        ref={ref}
+        aria-label={ariaLabel}
+        onClick={() => setOpen?.((prev) => !prev)}
+        sx={{
+          color: theme.palette.text.secondary,
+          borderColor: theme.palette.action.disabled,
+          minWidth: '245px',
+          fontSize: '15px',
+          position: 'relative',
+          zIndex: '1',
+        }}>
+        {displayString} <EventIcon sx={{marginLeft: '20px', color: theme.palette.text.secondary}} />
+      </Button>
       <Typography
         component={'span'}
         sx={{
