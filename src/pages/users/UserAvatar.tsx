4ccd7c2dea3b8153c768f0754fec6766bfdc18bf
amyjchen <amy.chen@discordapp.com>
2024-10-30 13:41:50 -0700

    limit UserAvatar to a fixed saturation/luminance

--- src/pages/users/UserAvatar.tsx
+++ src/pages/users/UserAvatar.tsx
@@ -2,23 +2,8 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 
 function stringToColor(string: string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
+  const hue = string.split('').reduce((acc, curr) => curr.charCodeAt(0) + acc, 0) % 360;
+  return `hsl(${hue}, 65%, 65%)`;
 }
 
 interface UserAvatarProps {
