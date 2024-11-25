59ec88d11813721becf2dcc252c9f1c3b061d3fe
Matthew Hintz <92115109+mhintz-clickup@users.noreply.github.com>
2024-11-25 14:11:34 -0500

    feat(conditional-access): changed this to work with tags as well as group names (#200)

--- examples/plugins/conditional_access/conditional_access.py
+++ examples/plugins/conditional_access/conditional_access.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import logging
+import os
 from typing import List, Optional
 
 import pluggy
@@ -11,6 +12,17 @@ from api.plugins import ConditionalAccessResponse
 request_hook_impl = pluggy.HookimplMarker("access_conditional_access")
 logger = logging.getLogger(__name__)
 
+# Constants for auto-approval conditions (not required if you only want to use the Auto-Approval TAG)
+# Example of how to set this in an environment variable in your .env.production file:
+# AUTO_APPROVED_GROUP_NAMES="Group1,Group2,Group3"
+AUTO_APPROVED_GROUP_NAMES = (
+    os.getenv("AUTO_APPROVED_GROUP_NAMES", "").split(",") if os.getenv("AUTO_APPROVED_GROUP_NAMES") else []
+)
+
+# Example of how to set this in an environment variable in your .env.production file:
+# AUTO_APPROVED_TAG_NAMES="Tag1,Tag2,Tag3"
+AUTO_APPROVED_TAG_NAMES = os.getenv("AUTO_APPROVED_TAG_NAMES", "Auto-Approve").split(",")
+
 
 @request_hook_impl
 def access_request_created(
@@ -18,11 +30,16 @@ def access_request_created(
 ) -> Optional[ConditionalAccessResponse]:
     """Auto-approve memberships to the Auto-Approved-Group group"""
 
-    if not access_request.request_ownership and group.name == "Auto-Approved-Group":
-        logger.info(f"Auto-approving access request {access_request.id} to group {group.name}")
-        return ConditionalAccessResponse(
-            approved=True, reason="Group membership auto-approved", ending_at=access_request.request_ending_at
-        )
+    if not access_request.request_ownership:
+        # Check either group name or tag for auto-approval
+        is_auto_approved_name = group.name in AUTO_APPROVED_GROUP_NAMES
+        is_auto_approved_tag = any(tag.name in AUTO_APPROVED_TAG_NAMES for tag in group_tags)
+
+        if is_auto_approved_name or is_auto_approved_tag:
+            logger.info(f"Auto-approving access request {access_request.id} to group {group.name}")
+            return ConditionalAccessResponse(
+                approved=True, reason="Group membership auto-approved", ending_at=access_request.request_ending_at
+            )
 
     logger.info(f"Access request {access_request.id} to group {group.name} requires manual approval")
 
