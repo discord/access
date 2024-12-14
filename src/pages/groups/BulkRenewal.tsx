28237c2ad81aeddaea58e692f55301f421265393
Rose Toomey <rose@gemini.com>
2024-12-14 11:33:32 +0100

    Make time dropdowns user configurable via an environmental config file path supplied as a build arg to Docker

--- src/pages/groups/BulkRenewal.tsx
+++ src/pages/groups/BulkRenewal.tsx
@@ -30,6 +30,7 @@ import {displayUserName, minTagTimeGroups, requiredReasonGroups} from '../../hel
 import {usePutGroupMembersById, PutGroupMembersByIdError, PutGroupMembersByIdVariables} from '../../api/apiComponents';
 import {GroupMember, OktaUserGroupMember, PolymorphicGroup, RoleGroupMap, RoleGroup} from '../../api/apiSchemas';
 import BulkRenewalDataGrid from '../../components/BulkRenewalDataGrid';
+import accessConfig from '../../config/accessConfig';
 
 interface Data {
   id: number;
@@ -68,24 +69,10 @@ interface CreateRequestForm {
   reason?: string;
 }
 
-const UNTIL_ID_TO_LABELS: Record<string, string> = {
-  '43200': '12 Hours',
-  '432000': '5 Days',
-  '1209600': 'Two Weeks',
-  '2592000': '30 Days',
-  '7776000': '90 Days',
-  indefinite: 'Indefinite',
-  custom: 'Custom',
-} as const;
-
-const UNTIL_JUST_NUMERIC_ID_TO_LABELS: Record<string, string> = {
-  '43200': '12 Hours',
-  '432000': '5 Days',
-  '1209600': 'Two Weeks',
-  '2592000': '30 Days',
-  '7776000': '90 Days',
-} as const;
-
+const UNTIL_ID_TO_LABELS: Record<string, string> = accessConfig.ACCESS_TIME_LABELS;
+const UNTIL_JUST_NUMERIC_ID_TO_LABELS: Record<string, string> = Object.fromEntries(
+  Object.entries(UNTIL_ID_TO_LABELS).filter(([key]) => !isNaN(Number(key))),
+);
 const UNTIL_OPTIONS = Object.entries(UNTIL_ID_TO_LABELS).map(([id, label], index) => ({id: id, label: label}));
 
 const RFC822_FORMAT = 'ddd, DD MMM YYYY HH:mm:ss ZZ';
@@ -113,7 +100,7 @@ function BulkRenewalDialog(props: BulkRenewalDialogProps) {
   const [selected, setSelected] = React.useState<OktaUserGroupMember[]>(() =>
     props.select != undefined ? props.rows.filter((r) => r.id == props.select) : [],
   );
-  const [until, setUntil] = React.useState('1209600');
+  const [until, setUntil] = React.useState(accessConfig.DEFAULT_ACCESS_TIME);
 
   const [selectionModel, setSelectionModel] = React.useState<GridRowSelectionModel>(() =>
     props.rows.filter((r) => r.id == props.select).map((r) => r.id),
