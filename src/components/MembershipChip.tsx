e89f41e71e3b3a4d158e20db54b0947513c5aa9e
eguerrant <elisa.guerrant@discordapp.com>
2025-06-03 11:59:24 -0700

    expiration date on hover

--- src/components/MembershipChip.tsx
+++ src/components/MembershipChip.tsx
@@ -3,9 +3,10 @@ import {useNavigate} from 'react-router-dom';
 import {useCurrentUser} from '../authentication';
 import {canManageGroup, isGroupOwner} from '../authorization';
 import Chip from '@mui/material/Chip';
+import Tooltip from '@mui/material/Tooltip';
 
 export interface MembershipChipProps {
-  user: OktaUserGroupMember;
+  okta_user_group_member: OktaUserGroupMember;
   group: PolymorphicGroup;
   removeRoleGroup: (roleGroup: RoleGroup) => void;
   removeDirectAccessAsUser: () => void;
@@ -13,7 +14,7 @@ export interface MembershipChipProps {
 }
 
 export default function MembershipChip({
-  user,
+  okta_user_group_member,
   group,
   removeRoleGroup,
   removeDirectAccessAsUser,
@@ -21,39 +22,47 @@ export default function MembershipChip({
 }: MembershipChipProps) {
   const navigate = useNavigate();
   const currentUser = useCurrentUser();
-  const activeRoleGroup = user.active_role_group_mapping?.active_role_group;
+  const activeRoleGroup = okta_user_group_member.active_role_group_mapping?.active_role_group;
+  const ending_date = okta_user_group_member.ended_at ?? 'Never';
   const canManageUserRoleGroup = activeRoleGroup?.id ? isGroupOwner(currentUser, activeRoleGroup.id) : false;
   const canManageThisGroup = group.is_managed && canManageGroup(currentUser, group);
-  const canManageThisUser = group.is_managed && currentUser.id === user.active_user?.id;
+  const canManageThisUser = group.is_managed && currentUser.id === okta_user_group_member.active_user?.id;
+
+  const moveTooltip = {modifiers: [{name: 'offset', options: {offset: [0, -10]}}]};
+
   return activeRoleGroup ? (
-    <Chip
-      label={activeRoleGroup.name}
-      variant="outlined"
-      color="primary"
-      onClick={() => navigate(`/roles/${activeRoleGroup.name}`)}
-      onDelete={
-        canManageThisGroup || canManageUserRoleGroup
-          ? () => {
-              removeRoleGroup(activeRoleGroup);
-            }
-          : undefined
-      }
-    />
-  ) : (
-    <Chip
-      label="Direct"
-      color="primary"
-      onDelete={
-        canManageThisUser
-          ? () => {
-              removeDirectAccessAsUser();
-            }
-          : canManageThisGroup
+    <Tooltip title={ending_date} placement="right" PopperProps={moveTooltip}>
+      <Chip
+        label={activeRoleGroup.name}
+        variant="outlined"
+        color="primary"
+        onClick={() => navigate(`/roles/${activeRoleGroup.name}`)}
+        onDelete={
+          canManageThisGroup || canManageUserRoleGroup
             ? () => {
-                removeDirectAccessAsGroupManager();
+                removeRoleGroup(activeRoleGroup);
               }
             : undefined
-      }
-    />
+        }
+      />
+    </Tooltip>
+  ) : (
+    <Tooltip title={ending_date} placement="right" PopperProps={moveTooltip}>
+      <Chip
+        label="Direct"
+        color="primary"
+        onDelete={
+          canManageThisUser
+            ? () => {
+                removeDirectAccessAsUser();
+              }
+            : canManageThisGroup
+              ? () => {
+                  removeDirectAccessAsGroupManager();
+                }
+              : undefined
+        }
+      />
+    </Tooltip>
   );
 }
