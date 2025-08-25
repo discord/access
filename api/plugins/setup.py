a67f547eac9c6db8cb281103b9a06bed1a23660e
Ian Uriegas <75506938+ianuriegas@users.noreply.github.com>
2025-08-25 16:59:48 -0500

    fix: Bump pluggy to 1.5.0 for plugins and examples (#330)

--- api/plugins/setup.py
+++ api/plugins/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="access-plugins",
-    install_requires=["pluggy==1.3.0"],
+    install_requires=["pluggy==1.5.0"],
     packages=find_packages(),
 )
